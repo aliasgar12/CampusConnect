dc368d627db33080337be0545656849bd865c799
Ali Asgar <ali.asgar26@gmail.com>
2017-04-21 21:03:15 -0400

    Configuring firebase with the project

---
+++ app/src/main/java/campusconnect/alias/com/campusconnect/firebase/MyFirebaseInstanceIdService.java
@@ -0,0 +1,27 @@
+package campusconnect.alias.com.campusconnect.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by alias on 4/21/2017.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private String TAG = "MyFireBaseService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
